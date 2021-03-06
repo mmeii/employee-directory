14b6728ca0ee7a96e9dd3a0c1da51b8d5959095f
Mengmei Tu <redmmeii@gmail.com>
2021-03-06 16:41:56 -0600

    styling on title

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Title() {
     return (
-        <div className="text-center bg-light p-5">
-            <h1>Staff Directory</h1>
-            <p>Employee Details</p>
+        <div className="text-center bg-info p-5">
+            <h1 style={{ fontSize: 60, color: "white" }}>Staff Directory</h1>
+            <p style={{ color: "white" }}>Employee Details</p>
         </div>
     )
 }
